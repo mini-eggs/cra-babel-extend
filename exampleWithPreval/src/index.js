c3574737b614382a2c46cbf2e6b71c3b23d6b50c
Evan <evan@crowdhubapps.com>
2017-08-13 18:02:08 -0700

    Simplifying preval example.

--- exampleWithPreval/src/index.js
+++ exampleWithPreval/src/index.js
@@ -1,24 +1,8 @@
 /* global preval */
-
 import React from "react";
 import { render } from "react-dom";
-import { Provider, connect } from "react-redux";
-import { createStore, combineReducers } from "redux";
 
-/**
- * Example component.
- */
-@connect(state => ({ version: state.ExampleReducer.version }))
-class ExampleComponent extends React.Component {
-  render() {
-    return <h1 children={`Example project version: ${this.props.version}`} />;
-  }
-}
-
-/**
- * Reducers.
- */
-const initialState = preval`
+const { version } = preval`
   const path = require("path");
   const fs = require("fs");
   const packageLocation = path.join(process.cwd(), "package.json");
@@ -27,19 +11,11 @@ const initialState = preval`
     version
   };
 `;
-function ExampleReducer(state = initialState) {
-  return state;
+
+class ExampleComponent extends React.Component {
+  render() {
+    return <h1 children={`Example project version: ${version}`} />;
+  }
 }
 
-/**
- * Configs.
- */
-const reducers = combineReducers({ ExampleReducer });
-const store = createStore(reducers);
-const app = <Provider store={store} children={<ExampleComponent />} />;
-const node = document.getElementById("root");
-
-/**
- * Run app.
- */
-render(app, node);
+render(<ExampleComponent />, document.getElementById("root"));
